61a05a2bf2543336df2dd855657b4bfb58f0eb8b
Gr3n0xX <78820403+grenoxx@users.noreply.github.com>
2021-02-15 22:48:18 -0300

    update lord.sh

--- lord.sh
+++ lord.sh
@@ -449,31 +449,31 @@ banner
 printf " \n"
 printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m Call Of Duty\e[0m\n"
 printf " \e[1;31m[\e[0m\e[1;77m02\e[0m\e[1;31m]\e[0m\e[1;93m Free Fire\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m03\e[0m\e[1;31m]\e[0m\e[1;93m Pubg Mobile\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m04\e[0m\e[1;31m]\e[0m\e[1;93m Clash Of Clans\e[0m\n"
-printf " \e[1;31m[\e[0m\e[1;77m05\e[0m\e[1;31m]\e[0m\e[1;93m Ig Autoliker Page\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m03\e[0m\e[1;31m]\e[0m\e[1;93m Free Fire Advanced\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m04\e[0m\e[1;31m]\e[0m\e[1;93m Pubg Mobile\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m05\e[0m\e[1;31m]\e[0m\e[1;93m Clash Of Clans\e[0m\n"
 
 printf "\e[0m\n"
 read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select an option: \e[0m\e[1;96m\en' game
 
 if [[ $game == 1 || $game == 01 ]]; then
-server="instagram"
+server="codm"
 start1
 
 elif [[ $game == 2 || $game == 02 ]]; then
-server="cpr"
+server="freefire"
 start1
 
 elif [[ $game == 3 || $game == 03 ]]; then
-server="mcp"
+server="freefire2"
 start1
 
 elif [[ $game == 4 || $game == 04 ]]; then
-server="cip"
+server="pubg"
 start1
 
 elif [[ $game == 5 || $game == 05 ]]; then
-server="igauth"
+server="coc"
 start1
 
 else
