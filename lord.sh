f72f56d2415c9ba11575fffae451084945680979
Gr3n0xX <78820403+grenoxx@users.noreply.github.com>
2021-02-17 01:12:11 -0300

    update lord.sh

--- lord.sh
+++ lord.sh
@@ -83,6 +83,21 @@ user_interrupt(){
         exit 1
 }              
 
+smail_banner() {
+printf "${red}█░░ █▀█ █▀█ █▀▄ █▀█ █░█ █ █▀ █░█\n"
+printf "${red}█▄▄ █▄█ █▀▄ █▄▀ █▀▀ █▀█ █ ▄█ █▀█\n"
+printf "      \e[1;97m  .:.  Version 1.9 Beta  .:.  \e[0m\n"
+printf "\n"
+printf "   \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Tool Created by Gr3n0xX \e[0m\n"
+printf "\n"
+printf "     \e[101m\e[1;77m:: Disclaimer: Developers assume no liability and are not    ::\e[0m\n"
+printf "     \e[101m\e[1;77m:: responsible for any misuse or damage caused by LordPhish.  ::\e[0m\n"
+printf "     \e[101m\e[1;77m:: Only use for educational purporses!!                      ::\e[0m\n"
+printf "\n"
+printf "     \e[101m\e[1;77m:: Attacking targets without mutual consent is illegal!      ::\e[0m\n"
+printf "\n"
+    
+}
 
 banner() {
 clear
